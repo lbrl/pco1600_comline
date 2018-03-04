247e432ab6e4856e3be08412644af0b4f1685319
liberulo <00000001h@gmail.com>
2018-03-04 19:21:01 +0900

    Check zombie and rm gosts.

--- raw2root.C
+++ raw2root.C
@@ -6,27 +6,26 @@
 
 int raw2root(string finname, string Foutname,
         int binx = 1, int biny = 1){
-    // std::ifstream fin("data/example_ifstream.bin", std::ios::binary);
-    // std::ifstream fin("data/example_ifstream2.bin", std::ios::binary);
-    // std::ifstream fin("data/example_ifstream3.bin", std::ios::binary);
-	// std::ifstream fin("/Users/liberulo/Dropbox/g-2/bpm/ccd_sdk/data/example_ifstream2.bin", std::ios::binary);
 	std::ifstream fin(finname.c_str(), std::ios::binary);
-    if( !fin )
+    if( !fin ){
         printf( "The file openning failed.\n" );
+        return 1;
+    }
     printf( "the size of char is %lu.\n", (sizeof(char)) );
     printf( "the size of short is %lu.\n", (sizeof(short)) );
     printf( "the size of Short_t is %lu.\n", (sizeof(Short_t)) );
     ////////////////
-    // TFile * Fout = new TFile("try5.root", "recreate");
     TFile * Fout = new TFile(Foutname.c_str(), "recreate");
+    if( Fout->IsZombie() ){
+        std::cout << "The output file is a zombie.\n";
+        delete Fout;
+        Fout = 0;
+        return 2;
+    }
     const int nx =  1600 / binx, ny = 1200 / biny;
-    // const int nx =  800, ny = 600;
     TH2S * h = new TH2S("h", "h", nx, 0, nx, ny, 0, ny);
     TTree * tout = new TTree("t", "Tree with pictures");
     tout->Branch("h", &h);
-    ////////////////
-    ////////////////
-    // fin.seekg(nx*ny*2, ios::cur);
     ////////////////
     unsigned short a[nx];
     while( !fin.eof() ){
@@ -38,11 +37,9 @@ int raw2root(string finname, string Foutname,
         }
         if( !fin.eof() ){
             tout->Fill();
-            std::cout << "pic\n";
         }
     }
     std::cout << "Reach the end of the file.\n";
-    // std::cout << "\n";
     ////////////////
     tout->Write();
     h->Write();
