b09f9d8939e144c7925a7158206cb3e1dde3e3c4
liberulo <00000001h@gmail.com>
2018-03-04 18:34:00 +0900

    IO parameter as names.

--- raw2root.C
+++ raw2root.C
@@ -1,19 +1,23 @@
 #include <TTree.h>
 #include <TFile.h>
-#include <TH1.h>
+#include <TH2S.h>
+#include <iostream>
+#include <fstream>
 
-{
+int raw2root(string finname, string Foutname){
     // std::ifstream fin("data/example_ifstream.bin", std::ios::binary);
     // std::ifstream fin("data/example_ifstream2.bin", std::ios::binary);
     // std::ifstream fin("data/example_ifstream3.bin", std::ios::binary);
-	std::ifstream fin("/Users/liberulo/Dropbox/g-2/bpm/ccd_sdk/data/example_ifstream2.bin", std::ios::binary);
+	// std::ifstream fin("/Users/liberulo/Dropbox/g-2/bpm/ccd_sdk/data/example_ifstream2.bin", std::ios::binary);
+	std::ifstream fin(finname.c_str(), std::ios::binary);
     if( !fin )
         printf( "The file openning failed.\n" );
     printf( "the size of char is %lu.\n", (sizeof(char)) );
     printf( "the size of short is %lu.\n", (sizeof(short)) );
     printf( "the size of Short_t is %lu.\n", (sizeof(Short_t)) );
     ////////////////
-    TFile * Fout = new TFile("try5.root", "recreate");
+    // TFile * Fout = new TFile("try5.root", "recreate");
+    TFile * Fout = new TFile(Foutname.c_str(), "recreate");
     const int nx =  1600, ny = 1200;
     // const int nx =  800, ny = 600;
     TH2S * h = new TH2S("h", "h", nx, 0, nx, ny, 0, ny);
@@ -39,8 +43,9 @@
     std::cout << "Reach the end of the file.\n";
     // std::cout << "\n";
     ////////////////
-    t->Write();
+    tout->Write();
     h->Write();
     Fout->Close();
     ////////////////
+    return 0;
 }
