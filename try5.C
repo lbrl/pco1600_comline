674d3992201ff4b3d968d8a167b1f24b30a708f8
liberulo <00000001h@gmail.com>
2018-03-04 18:07:09 +0900

    Save histograms to a tree.

--- try5.C
+++ try5.C
@@ -1,8 +1,12 @@
+#include <TTree.h>
+#include <TFile.h>
+#include <TH1.h>
+
 {
     // std::ifstream fin("data/example_ifstream.bin", std::ios::binary);
     // std::ifstream fin("data/example_ifstream2.bin", std::ios::binary);
     // std::ifstream fin("data/example_ifstream3.bin", std::ios::binary);
-	std::ifstream fin("example.txt", std::ios::binary);
+	std::ifstream fin("/Users/liberulo/Dropbox/g-2/bpm/ccd_sdk/data/example_ifstream2.bin", std::ios::binary);
     if( !fin )
         printf( "The file openning failed.\n" );
     printf( "the size of char is %lu.\n", (sizeof(char)) );
@@ -13,26 +17,29 @@
     const int nx =  1600, ny = 1200;
     // const int nx =  800, ny = 600;
     TH2S * h = new TH2S("h", "h", nx, 0, nx, ny, 0, ny);
-    /*
-    for(int i=0; i<ny; i++){
-        for(int j=0; j<nx; j++){
-            h->SetBinContent(j+1, ny-i, (short) 123);
-        }
-    }
-    */
+    TTree * tout = new TTree("t", "Tree with pictures");
+    tout->Branch("h", &h);
     ////////////////
     ////////////////
-    fin.seekg(nx*ny*2, ios::cur);
+    // fin.seekg(nx*ny*2, ios::cur);
     ////////////////
     unsigned short a[nx];
-    for(int i=0; i<ny; i++){
-        fin.read((char *) a, nx*2);
-        for(int j=0; j<nx; j++){
-            h->SetBinContent(j, ny-i, a[j]/4);
+    while( !fin.eof() ){
+        for(int i=0; i<ny; i++){
+            fin.read((char *) a, nx*2);
+            for(int j=0; j<nx; j++){
+                h->SetBinContent(j, ny-i, a[j]/4);
+            }
+        }
+        if( !fin.eof() ){
+            tout->Fill();
+            std::cout << "pic\n";
         }
     }
+    std::cout << "Reach the end of the file.\n";
     // std::cout << "\n";
     ////////////////
+    t->Write();
     h->Write();
     Fout->Close();
     ////////////////
